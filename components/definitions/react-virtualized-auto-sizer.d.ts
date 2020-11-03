cc2fd82c928f6949d0168273269499f9ab78e1ec
Pete Miller <miller.pete@gmail.com>
2020-11-02 22:23:19 -0800

    Avoid trusted types .innerHTML issues by using fork of react-virtualized-auto-sizer

---
+++ components/definitions/react-virtualized-auto-sizer.d.ts
@@ -0,0 +1,38 @@
+declare module '@brave/react-virtualized-auto-sizer' {
+  import * as React from 'react'
+  export interface Size {
+    height: number
+    width: number
+  }
+
+  export interface AutoSizerProps {
+    /** Function responsible for rendering children. */
+    children: (size: Size) => React.ReactNode
+
+    /** Optional custom CSS class name to attach to root AutoSizer element.    */
+    className?: string
+
+    /** Default height to use for initial render; useful for SSR */
+    defaultHeight?: number
+
+    /** Default width to use for initial render; useful for SSR */
+    defaultWidth?: number
+
+    /** Disable dynamic :height property */
+    disableHeight?: boolean
+
+    /** Disable dynamic :width property */
+    disableWidth?: boolean
+
+    /** Nonce of the inlined stylesheet for Content Security Policy */
+    nonce?: string
+
+    /** Callback to be invoked on-resize */
+    onResize?: (size: Size) => void
+
+    /** Optional inline style */
+    style?: React.CSSProperties
+  }
+
+  export default class extends React.Component<AutoSizerProps> {}
+}
